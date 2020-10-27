0d56dd20a834dddf817fb2077127dc74af948f57
Yuka <yukamoribe@outlook.com>
2020-10-27 12:18:07 -0400

    Add route to check user authentication upon login attempt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const bcrypt = require('bcrypt');
 
 const { User } = require('../../db/models');
 const { asyncHandler, hashPassword, handleValidationErrors } = require('../../utils');
@@ -7,43 +8,77 @@ const { generateToken, checkIfAuthenticated } = require('../../auth');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('users');
-});
+router.post('/auth', asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    res.locals.user = await User.findOne({ where: { email } });
+  } catch (e) {
+    next(e);
+  }
+
+  const isValidPassword = bcrypt.compareSync(password, res.locals.user.hashedPassword.toString());
+  if(isValidPassword) {
+    const { token } = generateToken(res.locals.user.id, res.locals.user.username);
+    return res.json({
+      token,
+      user: {
+        id: res.locals.user.id,
+        firstName: res.locals.user.firstName,
+        lastName: res.locals.user.lastName,
+        imageUrl: res.locals.user.imageUrl,
+        username: res.locals.user.username
+      }
+    });
+  }
+  const err = new Error('Invalid login information.');
+  err.title = "Invalid Login";
+  err.status = 401;
+  next(err);
+}));
 
 router.post('/', 
   validations,
   handleValidationErrors,
-  asyncHandler(async (req, res, next) => {
+  asyncHandler(async (req, res) => {
   const { username, firstName, lastName, email, password, profileImage } = req.body;
   const hashedPassword = await hashPassword(password.trim());
 
-  const user = await User.create({
-    username: username.trim(),
-    firstName: firstName.trim(),
-    lastName: lastName.trim(),
-    email: email.trim(),
-    hashedPassword,
-    profileImage: profileImage.trim()
-  });
+  try {
+    res.locals.user = await User.create({
+      username: username.trim(),
+      firstName: firstName.trim(),
+      lastName: lastName.trim(),
+      email: email.trim(),
+      hashedPassword,
+      profileImage: profileImage.trim()
+    });
+  } catch(e) {
+    next(e);
+  }
 
-  const { token } = generateToken(user.id, user.username);
+  const { token } = generateToken(res.locals.user.id, res.locals.user.username);
  
   res.status(201).json({
     token,
     user: {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      imageUrl: user.imageUrl,
-      username: user.username
+      id: res.locals.user.id,
+      firstName: res.locals.user.firstName,
+      lastName: res.locals.user.lastName,
+      imageUrl: res.locals.user.imageUrl,
+      username: res.locals.user.username
     }
   });
 }));
 
 router.get('/token', checkIfAuthenticated, (req, res) => {
+  const { id, username, firstName, lastName, profileImage: imageUrl } = res.locals.user;
   res.json({
-
+    id,
+    firstName,
+    lastName,
+    imageUrl,
+    username
   });
 });
 
